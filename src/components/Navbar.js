21b3a8ab3aff4381cb24cfdbf4648f3f818f3275
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-10-22 17:07:43 -0600

    Logo click fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button'
@@ -6,7 +6,7 @@ import { Button } from './Button'
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const[button, setButton] = useState(true);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
@@ -19,14 +19,19 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton()
+    }, [])
+
     window.addEventListener('resize', showButton);
 
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                Bryan <i className="fab fa-typo3" />
+            <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
+                Bryan 
+                <i className="fab fa-typo3" />
             </Link>
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -48,8 +53,7 @@ function Navbar() {
                     </Link>
                 </li>
             </ul>
-            {button && <Button buttonStyle='btn--outline'>Contact</
-            Button>}
+            {button && <Button buttonStyle='btn--outline'>Contact</Button>}
         </div>
     </nav>
 </>

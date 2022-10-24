0fc93ccd5278ae3aa793beaf23753e4338f7b051
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-10-23 19:52:57 -0600

    Footer.Js working and Footer.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,74 @@
-import React from 'react'
-import { Button } from './Button'
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className="footer-container">
-        <section className="footer-subscription">
-            <p className="footer-subscription-heading">
-                Join the adeventure
-            </p>
-            <p className="footer-subscription-text">
-                You can unsuscribe at any time.
-            </p>
-            <div className="input-areas">
-                <form>
-                    <input type="email" name="email" placeholder='Your Email' className='footer-input'/>
-                    <Button buttonStyle='btn--outline'>Suscribe</Button>
-                </form>
-            </div>
-        </section>
-        <div className="footer-links">
-            <div className="footer-link-wrapper">
-                <div className="-link-items">
-                    <Link to='/about'>
-                        <h2>About Me</h2>
-                    </Link>
-                </div>
-                <div className="-link-items">
-                    <Link to='/about'>
-                        <h2>Work</h2>
-                    </Link>
-                </div>
-                <div className="-link-items">
-                    
-                    <Link to='/about'>
-                        <h2>Contact Me</h2>
-                    </Link>
-                </div>
-            </div>
+    <div className='footer-container'>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+           <Link to='/about'><h2>About Me</h2></Link>
+          </div>
+          <div class='footer-link-items'>
+          <Link to='/about'><h2>Portfolio</h2></Link>
+          </div>
         </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+          <Link to='/about'><h2>Contact</h2></Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              Bryan Burciaga
+            </Link>
+          </div>
+          <small class='website-rights'>Bryan Burciaga © 2022</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+      
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='https://www.linkedin.com/in/bryanburciaga/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+    
+  );
 }
 
 export default Footer;
